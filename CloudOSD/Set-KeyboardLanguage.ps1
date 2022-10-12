5c66c017e3c49f879986ce910cf86b91dd331e3f
Martin Felder <martin.felder@outlook.com>
2022-10-12 09:19:05 +0200

    Create Set-KeyboardLanguage.ps1

---
+++ CloudOSD/Set-KeyboardLanguage.ps1
@@ -0,0 +1,12 @@
+$LanguageList = Get-WinUserLanguageList
+
+$LanguageList.Add("de-CH")
+Set-WinUserLanguageList $LanguageList -Force
+
+Start-Sleep -Seconds 5
+
+$LanguageList = Get-WinUserLanguageList
+$LanguageList.Remove(($LanguageList | Where-Object LanguageTag -like 'de-DE'))
+Set-WinUserLanguageList $LanguageList -Force
+
+$LanguageList = Get-WinUserLanguageList
