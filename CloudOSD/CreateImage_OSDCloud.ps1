7e6b6d31bd1f93ec05cdcf7ac2bb0752fee3da96
Martin Felder <martin.felder@outlook.com>
2024-01-03 14:06:25 +0100

    Create CreateImage_OSDCloud.ps1

---
+++ CloudOSD/CreateImage_OSDCloud.ps1
@@ -0,0 +1,13 @@
+# Set Workshpace Folder
+$WorkingDir = "D:\OSDCloud"
+New-Item -ItemType Directory $WorkingDir -Force
+Set-OSDCloudWorkspace -WorkspacePath $WorkingDir
+
+#Create OSD Cloud Template en-us with Swiss Inpute locale
+New-OSDCloudTemplate -SetInputLocale de-CH -Name 'Template EN-US - InputLocal de-CH' -WinRE -Verbose
+
+$Startnet = @'
+start /wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
+start /wait PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail
+'@
+Edit-OSDCloudWinPE -Startnet $Startnet -StartOSDCloudGUI -Brand 'OSD by Martin' -Wallpaper "D:\OneDrivePrivat\OneDrive\_b41d5361-7e1f-4abd-823e-c5f43da6e67c.jpg" -CloudDriver * -DriverPath "D:\OneDrivePrivat\OneDrive\Drivers"
