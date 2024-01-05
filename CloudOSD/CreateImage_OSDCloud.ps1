641b598bf905d4cb4ddaa130d4cb231e3483a6e7
Martin Felder <martin.felder@outlook.com>
2024-01-05 06:59:24 +0100

    Update CreateImage_OSDCloud.ps1

--- CloudOSD/CreateImage_OSDCloud.ps1
+++ CloudOSD/CreateImage_OSDCloud.ps1
@@ -13,3 +13,5 @@ start /wait PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 2
 Edit-OSDCloudWinPE -Startnet $Startnet -StartOSDCloudGUI -Brand 'OSD by Martin' -Wallpaper "D:\OneDrivePrivat\OneDrive\_b41d5361-7e1f-4abd-823e-c5f43da6e67c.jpg" -CloudDriver * -DriverPath "D:\OneDrivePrivat\OneDrive\Drivers"
 
 Edit-OSDCloudWinPE -CloudDriver * -StartOSDCloud "-OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage en-us -OSLicense Retail" -UpdateUSB -Wallpaper "D:\OneDrivePrivat\OneDrive\_b41d5361-7e1f-4abd-823e-c5f43da6e67c.jpg" -Brand "MFLAB OSDCloud" -DriverPath "D:\OneDrivePrivat\OneDrive\Drivers"
+
+Edit-OSDCloudWinPE -startosdpad "-RepoOwner martinfelder -RepoName OSD -RepoFolder CloudOSD -BrandingTitle 'MFLAB OSD Cloud Deployment' -Hide Script"
