bed17d64b1030ac3f77a3791bfcb9f6603ac7f2a
Martin Felder <martin.felder@outlook.com>
2025-06-05 16:00:25 +0200

    Disable Cleanup

--- CloudOSD/Win25H2.ps1
+++ CloudOSD/Win25H2.ps1
@@ -319,7 +319,7 @@ Write-DarkGrayHost "Downloading Scripts for OOBE and specialize phase"
 
 Invoke-RestMethod https://raw.githubusercontent.com/martinfelder/OSD/refs/heads/main/CloudOSD/Autopilot.ps1| Out-File -FilePath 'C:\Windows\Setup\scripts\autopilot.ps1' -Encoding ascii -Force
 Invoke-RestMethod http://oobe.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\oobe.ps1' -Encoding ascii -Force
-Invoke-RestMethod http://cleanup.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
+# Invoke-RestMethod http://cleanup.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\cleanup.ps1' -Encoding ascii -Force
 #Invoke-RestMethod http://osdgather.osdcloud.ch | Out-File -FilePath 'C:\Windows\Setup\scripts\osdgather.ps1' -Encoding ascii -Force
 
 $OOBEcmdTasks = @'
